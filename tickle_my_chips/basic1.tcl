0e1b679b867545a95a7f8dbe0fadc350772e826d
Ram Tripathi <ram0183rt@gmail.com>
2025-09-15 08:19:16 +0000

    modify files

--- tickle_my_chips/basic1.tcl
+++ tickle_my_chips/basic1.tcl
@@ -9,7 +9,7 @@ puts "Welcome to my journey in VLSI especially Physical Design"
 #expr, $ is to print the value of a variable
 
 set a 5
-set b 6
+set b 1
 puts "the value of a is $a"
 set c [expr $a + $b]
 puts "The sum of a and b is $c"
@@ -18,19 +18,24 @@ puts "The sum of a and b is $c"
 
 #Arithmetic
 set a 5
-set b 4
+set b 10
 set result [expr $a + $b]
+set resultdiff [expr $a - $b]
+set resultmult [expr $a * $b]
+set resultdiv [expr $a / $b]
+set exponent [expr $a ** $b]
 # - * % ^
 puts "Sum: $result"
-
+puts "Diff: $resultdiff"
+puts "Multiplication: $resultmult"
+puts "Division : $resultdiv"
+puts "Exponent : $exponent"
 #Rotational ==,!=, <,>, <=, > =
 
-#Arithmetic
-set a 5
-set b 3
-set result [expr $a + $b]
-# - * / %^
-puts "Sum: $result"
+
+
+
+
 
 // set mylist {1 2 3 4 5}
 // set first {lindex $mylist 0}
