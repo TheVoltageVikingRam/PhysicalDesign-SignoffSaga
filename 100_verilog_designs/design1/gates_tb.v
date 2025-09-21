fce51d603e4d0d9d79bb0df0da8137154d0306e8
Ram Tripathi <ram0183rt@gmail.com>
2025-09-22 02:02:58 +0530

    Add files via upload

---
+++ 100_verilog_designs/design1/gates_tb.v
@@ -0,0 +1,17 @@
+module gates_tb();
+
+reg a,b;
+wire c,d,e,f,g,h,i;
+gates uut ( .a(a), .b(b), .c(c), .d(d), .e(e), .f(f), .g(g), .h(h), .i(i) );
+
+initial begin
+a=0; b=0; #5;
+a=1; b=0; #5;
+a=1; b=1; #5;
+a=0; b=1; #5;
+a=0; b=0; #5;
+a=1; b=1; #5;
+$finish;
+end
+endmodule
+
