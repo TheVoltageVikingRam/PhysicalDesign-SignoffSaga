004712dfd643321a6ea4b9d8cc246746d328c562
Ram Tripathi <ram0183rt@gmail.com>
2026-03-26 19:17:19 +0530

    Update genus_script.tcl

--- RTL_to_GDSII_projects/SIMPLE_COUNTER/synthesis/genus_script.tcl
+++ RTL_to_GDSII_projects/SIMPLE_COUNTER/synthesis/genus_script.tcl
@@ -1,9 +1,9 @@
-                                                               
-set_db init_lib_search_path /home/server2/cadence/installs/DDI231/INNOVUS231/share/FoundationFlows/EXAMPLES/INNOVUS/DESIGN/GPDK/LIBS/GPDK045/timing/
-set_db init_hdl_search_path /home/server2/Music/ram/counter
-read_libs "fast.lib  slow.lib  typical.lib"
 
-read_hdl counter.v
+set_db init_lib_search_path /home/server2/design_kits/pd_cadence/BlockImplementation231/RAK_23.10_blockImplementation/LIBS/lib/max  #change according to yours
+set_db init_hdl_search_path /home/server2/Music/ram/counter #give your path
+read_libs {slow.lib leon.lib} 
+
+read_hdl -language v2001 counter.v
 elaborate
 read_sdc ../constraints/constraints_top.sdc
 
@@ -12,13 +12,15 @@ set_db syn_map_effort medium
 set_db syn_opt_effort medium
 
 syn_generic
+check_timing_intent
+check_design -unresolved
 syn_map
 syn_opt
 
 #reports
 
 report_timing > reports/report_timing.rpt
-report_power > reports/report_area.rpt
+report_power > reports/report_power.rpt
 report_area > reports/report_area.rpt
 report_qor > reports/report_qor.rpt
 
@@ -27,6 +29,9 @@ report_qor > reports/report_qor.rpt
 write_hdl > outputs/counter_netlist.v
 write_sdc > outputs/counter_sdc.sdc
 write_sdf -timescale ns -nonegchecks -recrem split -edges check_edge -setuphold split > outputs/delays.sdf
+
+
+
 
 
 
