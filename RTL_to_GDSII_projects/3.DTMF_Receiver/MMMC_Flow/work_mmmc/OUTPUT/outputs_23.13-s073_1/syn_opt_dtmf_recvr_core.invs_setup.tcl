22b39bc33451e059b387bdbf2fb0e10cbe13e95f
Ram Tripathi <ram0183rt@gmail.com>
2026-06-23 20:02:54 +0000

    add output files via upload from mmmc functional mode synthesis in genus

---
+++ RTL_to_GDSII_projects/3.DTMF_Receiver/MMMC_Flow/work_mmmc/OUTPUT/outputs_23.13-s073_1/syn_opt_dtmf_recvr_core.invs_setup.tcl
@@ -0,0 +1,6 @@
+
+          #   Created by Genus(TM) Synthesis Solution 23.13-s073_1
+          #   on 06/24/2026 01:11:28
+          #
+          read_db -common OUTPUT/outputs_23.13-s073_1/syn_opt_dtmf_recvr_core.cdb
+        
