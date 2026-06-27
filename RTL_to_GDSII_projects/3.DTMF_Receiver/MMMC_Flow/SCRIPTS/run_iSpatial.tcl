7f7a8076811bbcae4b403744b4319cd51a0cc8b2
Ram Tripathi <ram0183rt@gmail.com>
2026-06-27 20:42:44 +0530

    Enhance run_iSpatial.tcl with directory checks and comments
    Added checks to create directories for outputs, reports, and logs if they do not exist. Enhanced the script setup for iSpatial with additional comments and settings.

--- RTL_to_GDSII_projects/3.DTMF_Receiver/MMMC_Flow/SCRIPTS/run_iSpatial.tcl
+++ RTL_to_GDSII_projects/3.DTMF_Receiver/MMMC_Flow/SCRIPTS/run_iSpatial.tcl
@@ -33,4 +33,73 @@ set _REPORTS_PATH iSpatial_reports ; #Directory name for reports
 set _LOGS_PATH iSpatial_logs ; #Setting directory name for logs
 
 #Creating directory name for outputs, reports and logs if not present already based on the variables set at the initial stage 
+if {![file exists ${_LOG_PATH}]} {
+    file mkdir ${_LOG_PATH}
+    puts "Creating directory ${_LOG_PATH}"
+}
+
+
+if {![file exists ${_OUTPUTS_PATH}]} {
+    file mkdir ${_OUTPUTS_PATH}
+    puts "Creating directory ${_OUTPUTS_PATH}"
+
+}
+
+
+if {![file exists ${_REPORTS_PATH}]} {
+    file mkdir ${_REPORTS_PATH}
+    puts "Creating directory ${_REPORTS_PATH} "
+
+}
+
+regexp \[0-9\]+(\.\[0-9\]+) [get_db program_version] exe_ver exe_sub_ver
+
+puts "Executable Version : $exe_ver"
+set_db information_level 9    ; # Controls the amount of information genus produces when executing commands, Specify an integer number from 0 to 9, higher the number higher the verbosity
+
+######################
+## iSpatial setup ##
+######################
+
+
+
+#Enable iSpatial 
+set_db opt_spatial_effort extreme
+
+
+#IMPORTANT : specify Innovus executable by uncommenting the following and filling in your Innovus command path
+
+#NOT requried if you have your PATH env variable set to find the innovus command
+#set_db innovus_executable  <path to innovus executable>
+
+
+#Optional power optimization settings
+#set_db design_power_effort high
+
+
+
+########################################
+## Library and MMMC Settings ##
+########################################
+
+
+read_mmmc ../SCRIPTS/mmmc_iSpatial.tcl
+read_physical -lefs [gsclib045_tech.lef gsclib045_macro.lef pllclk.lef CDK_S128x16.lef CDK_S256x16.lef CDK_R512x16.lef}
+
+
+#########################################
+## Load Design and Initialize ##
+#########################################
+
+#Making a list of all hdl files to be read in
+
+
+puts "Now load RTL"
+
+
+
+
+
+
+
 
